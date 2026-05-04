b78a9460a0b880128fae82c0d34b43a6537c98ab
buppou-git <h_kan0105@icloud.com>
2026-05-05 00:55:50 +0900

    解決?

--- app/japanese-holidays.d.ts
+++ app/japanese-holidays.d.ts
@@ -3,7 +3,7 @@ declare module 'japanese-holidays' {
     /**
      * 指定した日付が祝日であれば祝日名を、祝日でなければ undefined を返します。
      */
-    export function isJPHoliday(date: Date): string | undefined;
+    export function isHoliday(date: Date): string | undefined;
   
     /**
      * 指定した年の祝日一覧を取得します。
